eb9535d8e13f95e8084071bf9a73f7e4009c5c4e
Lauren <technical.me@aol.com>
2018-08-24 21:32:07 -0400

    done

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@
  use burger_db;
  
  create table burgers (
- id int not null auto_increment primary key,
- burger_name varchar(100),
- devoured boolean false,
+ id int auto_increment primary key,
+ burger_name varchar(100) not null,
+ devoured boolean DEFAULT false,
  date TIMESTAMP
  );
